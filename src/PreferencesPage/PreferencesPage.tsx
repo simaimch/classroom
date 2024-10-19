6ffef0acb3b83c85dc598e9008abbd8bdb65d3d1
Christian Hoffmann <christian.fr.hoffmann@gmail.com>
2024-10-19 15:06:33 +0200

    pretty up PreferencePage

--- src/PreferencesPage/PreferencesPage.tsx
+++ src/PreferencesPage/PreferencesPage.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import MenuBar from "../_ui/MenuBar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AccountContext } from "../_contexts/AccountContext";
 import parseName from "../_ui/parseName";
 import { SetAccount } from "../App";
 import updateObject from "../_helpers/updateObject";
 import Account from "../_types/Account";
 
+import './PreferencesPage.css';
+
 export default function PreferencesPage(){
     
 	const account = useContext(AccountContext);
+
+    const [studentLabelExample, setStudentLabelExample] = useState<string>("Musterschüler, Hannah Maximilian (Hama)");
 
     const studentLabelingOptions:{
         label: string,
@@ -27,8 +31,6 @@ export default function PreferencesPage(){
             description: 'Den Spitznamen des Schülers darstellen. Dieser ist in Klammern aufgeführt. Falls kein Spitzname vorhanden ist, wird der erste Vorname des Schülers verwendet.'
         }
     ]; 
-
-    const exampleName = "Musterschüler, Hannah Maximilian (Hama)";
     
     const studentLabelingOptionInputs = studentLabelingOptions.map((option)=>{
         if(!account)
@@ -43,7 +45,7 @@ export default function PreferencesPage(){
                 />
                 <label>{option.label}</label>
                 <span>{option.description}</span>
-                <span>Beispiel: {parseName(exampleName,option.value)}</span>
+                <span>Beispiel: {parseName(studentLabelExample,option.value)}</span>
             </div>
         );
     });
@@ -54,8 +56,12 @@ export default function PreferencesPage(){
                 <Link to={"/"}>Startseite</Link>
             </MenuBar>
             <h1>Einstellungen</h1>
-            <h2>Schüler-Namen-Anzeige</h2>
-            {studentLabelingOptionInputs}
+            <h2></h2>
+            <details>
+                <summary>Schüler-Namen-Anzeige</summary>
+                <p>Beispiel-Name: <input defaultValue={studentLabelExample} onChange={(e)=>setStudentLabelExample(e.target.value)}></input></p>
+                {studentLabelingOptionInputs}
+            </details>
         </div>
     );
 }
